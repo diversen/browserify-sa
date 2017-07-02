c20620c966f455d60e90ca5be3efe9830001eee7
Dennis Iversen <dennis.iversen@gmail.com>
2017-07-02 17:42:59 +0200

    auto commit

---
+++ cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+var cwd = process.cwd()
+var file = cwd + '/package.json'
+var json = require(file);
+var command = 'mkdir -p dist && browserify ' + json.main + ' -s ' + json.name + ' > dist/' + json.name + '.js';
+
+var exec = require('child_process').exec;
+
+exec(command, function(error, stdout, stderr) {
+    console.log(command);
+    if (error)console.log(error);
+    if (stderr)console.log(stderr);
+});
+
+
---
+++ index.js
@@ -0,0 +1,12 @@
+var cwd = process.cwd()
+var file = cwd + '/package.json'
+var json = require(file);
+var command = 'mkdir -p dist && browserify ' + json.main + ' -s ' + json.name + ' > dist/' + json.name + '.js';
+
+var exec = require('child_process').exec;
+
+exec(command, function(error, stdout, stderr) {
+    console.log(command);
+    if (error)console.log(error);
+    if (stderr)console.log(stderr);
+});
