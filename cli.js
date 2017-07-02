4d94052029e4a3290aa01e8e793d3bfb21ba8a78
Dennis Iversen <dennis.iversen@gmail.com>
2017-07-02 18:49:01 +0200

    add info

--- cli.js
+++ cli.js
@@ -4,9 +4,28 @@ var cwd = process.cwd()
 var file = cwd + '/package.json'
 var json = require(file);
 var command = 'mkdir -p dist &&';
-command+=' browserify ' + json.main + '  -s ' + json.name;
-command+= ' |  uglifyjs  > dist/' + json.name + '.js';
 
+var infoAry = new Array();
+infoAry.push("Package: " + json.name);
+infoAry.push("Version: " + json.version);
+if (json.license) {
+    infoAry.push("License: " + json.license);
+}
+
+if (json.author) {
+    infoAry.push( "Author: " + json.author);
+}
+
+if (json.homepage) {
+    infoAry.push( "Homepage: " + json.homepage);
+}
+
+var infoStr = infoAry.join('. ')
+
+command+= "(printf  '/*" +infoStr +"   */ '";
+command+=' && browserify ' + json.main + '  -s ' + json.name;
+command+= ' |  uglifyjs ) ' 
+command+= ' > dist/' + json.name + '.js';
 
 var exec = require('child_process').exec;
 
