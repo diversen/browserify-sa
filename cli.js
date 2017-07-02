9ba5ac2bee57ecfa87e7564671d72857069a0325
Dennis Iversen <dennis.iversen@gmail.com>
2017-07-02 17:52:46 +0200

    uglify

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 var cwd = process.cwd()
 var file = cwd + '/package.json'
 var json = require(file);
-var command = 'mkdir -p dist && browserify ' + json.main + ' -s ' + json.name + ' > dist/' + json.name + '.js';
+var command = 'mkdir -p dist && browserify ' + json.main + '  -s ' + json.name + ' |  uglifyjs  > dist/' + json.name + '.js';
 
 var exec = require('child_process').exec;
 
